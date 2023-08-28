a7d6fc0b97873501a3ebe3288597d220880ec0c5
Vinh Nguyen <vinhnguyenpk.it@gmail.com>
2023-08-28 22:36:29 +0700

    Remove unnecessary files and configurations

--- projects/admin-web/src/pages/Admin.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { PageContainer } from "@ant-design/pro-layout";
-
-export default (props: any): React.ReactNode => {
-  return <PageContainer>{props.children}</PageContainer>;
-};
