31c03350496a0cdca38339459fdeb1be09c5c8d7
Vinh Nguyen <vinhnguyenpk.it@gmail.com>
2023-08-29 14:10:14 +0700

    feat: Update package dependencies and configuration
    This commit updates the following:
    
    - Added `.prettierignore` file to

--- projects/client-web/src/app/app.routes.ts
+++ projects/client-web/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+    pathMatch: 'full',
+  },
+  // {
+  //   path: 'products',
+  //   component: ProductsComponent
+  // },
+];
