208ab3a2aae6fc620b1cfd095f33ccd2eefef286
Vinh Nguyen <vinhnguyenpk.it@gmail.com>
2023-08-31 02:30:41 +0700

    Refactor code structure and imports in admin-api.module.ts, app.module.ts, database.module.ts
    - Refactored import statements in admin-api.module.ts to use single quotes instead of double quotes.
    - Removed unnecessary imports and controllers from admin-api.module.ts.
    - Refactored import statements in app.module.ts to use single quotes instead of double quotes.
    - Removed unnecessary imports and middleware from app.module.ts.
    - Refactored import statements in database.module.ts to use single quotes instead of double quotes.
    - Removed unnecessary imports and entities from database.module.ts.
    
    This commit refactors the code structure and improves the readability of the affected modules.

--- projects/api/src/musculoskeletal-admin-api/musculoskeletal-admin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { MusculoskeletalModule } from "../musculoskeletal/musculoskeletal.module";
-import { AdminMusculoskeletalController } from "./musculoskeletal-admin.controller";
-
-@Module({
-  imports: [MusculoskeletalModule],
-  controllers: [AdminMusculoskeletalController],
-})
-export class MusculoskeletalAdminApiModule {}
